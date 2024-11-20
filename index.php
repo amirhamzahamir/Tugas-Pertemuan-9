59e1fa5f2a9e9106d757c9291dd1b983d0ce10d3
amirhamzahamir <amirhmzh0420@gmail.com>
2024-11-20 22:04:51 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$Nama ="Amir Hamzah";
+$Nim ="312310324";
+$Program ="Teknik informatika";
+
+?>
+
+<h1>
+    Perkenalan Diri Saya
+</h1>
+<p>
+Nama :<?php echo $Nama?>
+<p>
+Nim :<?php echo $Nim?>
+<p> 
+Program :<?php echo $Program?>
+</p>
